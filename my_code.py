62acda07fd7c644c2487aa68b988afbab7e9aa91
maleich <mleich@athenian.org>
2020-08-09 17:53:41 -0700

    create hello world assignment & 2 tests

--- my_code.py
+++ my_code.py
@@ -1,16 +1,20 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
-def inc(x):
-    # User code goes here
-    return x + 1
+def greeting(name):
+    # User code goes here.
+    # Your function should print a greeting that looks like: 'Hello, Cara!' (using `name`)
+   print
+
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(inc(6))
+    print(greeting('Cara'))
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
+    # After you are satisfied with your results, use input() to prompt the user for a name.
+    # Then use that name as an argument in your function.
+
+    #user = input("What is your name? ")
+    #print(greeting(user))
--- my_test.py
+++ my_test.py
@@ -1,7 +1,16 @@
-from my_code import inc
+import pytest
+from my_code import *
 
+global name, name2
+name1 = 'Brianna'
+name2 = 'Newton'
 
-def test_inc():
-    assert 5 == inc(4)
-    assert 1 == inc(0)
-    assert 0 == inc(-1)
+def test_greeting1(capsys):
+    greeting(name1)
+    captured = capsys.readouterr()
+    assert captured.out == f"Hello, {name1}!\n"
+
+def test_greeting2(capsys):
+    greeting(name2)
+    captured = capsys.readouterr()
+    assert captured.out == f"Hello, {name2}!\n"
